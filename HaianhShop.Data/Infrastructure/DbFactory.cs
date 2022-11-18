059cb99f4f0d7ea39abca544d04553f2c19717f0
haianh245 <tysunhb1234@gmail.com>
2022-11-18 10:36:35 +0700

    add entity framework code first for data

---
+++ HaianhShop.Data/Infrastructure/DbFactory.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HaianhShop.Data.Infrastructure
+{
+    public class DbFactory : Disposeable, IDbFactory
+    {
+        private HaiAnhShopDbContext dbContext;
+
+        public HaiAnhShopDbContext Init()
+        {
+            return dbContext ?? (dbContext = new HaiAnhShopDbContext());
+        }
+
+        protected override void DisposeCore()
+        {
+            if (dbContext != null)
+                dbContext.Dispose();
+        }
+    }
+}
