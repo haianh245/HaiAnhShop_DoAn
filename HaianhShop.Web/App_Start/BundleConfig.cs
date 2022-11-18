d06672abe7169e64bf2e96f5c2b98079025710e7
haianh245 <tysunhb1234@gmail.com>
2022-11-18 14:34:45 +0700

    edit web and unit test

--- HaianhShop.Web/App_Start/BundleConfig.cs
+++ HaianhShop.Web/App_Start/BundleConfig.cs
@@ -1,7 +1,7 @@
 ﻿using System.Web;
 using System.Web.Optimization;
 
-namespace HaianhShop.Wed
+namespace HaianhShop.Web
 {
     public class BundleConfig
     {
--- HaianhShop.Web/Controllers/HomeController.cs
+++ HaianhShop.Web/Controllers/HomeController.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
-namespace HaianhShop.Wed.Controllers
+namespace HaianhShop.Web.Controllers
 {
     public class HomeController : Controller
     {
