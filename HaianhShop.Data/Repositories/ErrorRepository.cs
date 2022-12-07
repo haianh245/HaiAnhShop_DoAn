13636fef7b4357747788dc1f7fd9c0383039aeb0
haianh245 <tysunhb1234@gmail.com>
2022-12-07 22:22:19 +0700

    add web api

---
+++ HaianhShop.Data/Repositories/ErrorRepository.cs
@@ -0,0 +1,16 @@
+﻿using HaianhShop.Data.Infrastructure;
+using HaianhShop.Model.Models;
+
+namespace HaianhShop.Data.Repositories
+{
+    public interface IErrorRepository : IRepository<Error>
+    {
+    }
+
+    public class ErrorRepository : RepositoryBase<Error>, IErrorRepository
+    {
+        public ErrorRepository(IDbFactory dbFactory) : base(dbFactory)
+        {
+        }
+    }
+}
