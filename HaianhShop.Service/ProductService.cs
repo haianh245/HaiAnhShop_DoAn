b8be0017dd2e962f5dd6dc0c8b413e3f82f036a0
haianh245 <tysunhb1234@gmail.com>
2022-12-22 20:18:05 +0700

    search and autocomplete

--- HaianhShop.Service/ProductService.cs
+++ HaianhShop.Service/ProductService.cs
@@ -24,6 +24,9 @@ namespace HaianhShop.Service
         IEnumerable<Product> GetHotProduct(int top);
 
         IEnumerable<Product> GetListProductByCategoryIdPaging(int categoryId, int page, int pageSize, string sort, out int totalRow);
+        IEnumerable<Product> Search(string keyword, int page, int pageSize, string sort, out int totalRow);
+        IEnumerable<string> GetListProductByName(string name);
+
         Product GetById(int id);
 
         void Save();
@@ -159,7 +162,34 @@ namespace HaianhShop.Service
                     break;
             }
             totalRow = query.Count();
+            return query.Skip((page - 1) * pageSize).Take(pageSize);
+        }
 
+        public IEnumerable<string> GetListProductByName(string name)
+        {
+            return _productRepository.GetMulti(x => x.Status && x.Name.Contains(name)).Select(y => y.Name);
+        }
+
+        public IEnumerable<Product> Search(string keyword, int page, int pageSize, string sort, out int totalRow)
+        {
+            var query = _productRepository.GetMulti(x => x.Status && x.Name.Contains(keyword));
+
+            switch (sort)
+            {
+                case "popular":
+                    query = query.OrderByDescending(x => x.ViewCount);
+                    break;
+                case "discount":
+                    query = query.OrderByDescending(x => x.PromotionPrice.HasValue);
+                    break;
+                case "price":
+                    query = query.OrderBy(x => x.Price);
+                    break;
+                default:
+                    query = query.OrderByDescending(x => x.CreatedDate);
+                    break;
+            }
+            totalRow = query.Count();
             return query.Skip((page - 1) * pageSize).Take(pageSize);
         }
     }
